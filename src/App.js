ceb55f30923945db5e09020ceb42f74504f53eb2
Shivika2934 <shivikamittal@ymail.com>
2024-06-09 12:59:36 +0530

    Added links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/Shivika2934"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Shivika Mittal
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Shivika2934/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
