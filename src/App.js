348820ea9f193ef6eb7e837b558dc9f3ac501bb1
Shivika2934 <shivikamittal@ymail.com>
2024-06-09 17:36:34 +0530

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/Shivika2934"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Shivika Mittal
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Shivika2934/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Shivika2934"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Shivika Mittal
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Shivika2934/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
